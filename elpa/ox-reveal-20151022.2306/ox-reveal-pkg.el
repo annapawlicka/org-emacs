646a1e1a1668c3e40e871fd3e3ec602c5590375c
Anna Pawlicka <pawlicka.anna@gmail.com>
2016-03-02 14:04:55 -0800

    Change how windows are moved + upgrade packages

--- elpa/ox-reveal-20151022.2306/ox-reveal-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "ox-reveal" "20151022.2306" "reveal.js Presentation Back-End for Org Export Engine" '((org "20150330")) :keywords '("outlines" "hypermedia" "slideshow" "presentation"))
