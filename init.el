9a4f77c96d8a61d6886bd72d51e0b79dcd28610b
annapawlicka <pawlicka.anna@gmail.com>
2020-08-21 19:14:20 -0700

    Fix loading of use-package, tidy up stuff.

--- init.el
+++ init.el
@@ -5,59 +5,104 @@
 ;; here:
 ;;
 ;; http://orgmode.org/worg/org-contrib/babel/intro.html
-(require 'package)
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-			 ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("org" . "http://orgmode.org/elpa/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")
-                        ;; ("melpa-stable" . "http://melpa-stable.milkbox.net/packages/")
-                         ))
 
-;; This means we prefer things from ~/.emacs.d/elpa over the standard packages.
+(require 'package)
+(setq package-enable-at-startup nil)
+
+;; https://emacs.stackexchange.com/a/2989
+(setq package-archives
+      '(("elpa"     . "https://elpa.gnu.org/packages/")
+        ("melpa-stable" . "https://stable.melpa.org/packages/")
+        ("melpa"        . "https://melpa.org/packages/")
+	("org" . "http://orgmode.org/elpa/")
+	("gnu" . "http://elpa.gnu.org/packages/"))
+      package-archive-priorities
+      '(("melpa-stable" . 10)
+        ("elpa"     . 5)
+        ("melpa"        . 0)))
+
 (package-initialize)
 
-;; This bootstraps us if we don't have anything
-(when (not package-archive-contents)
-  (package-refresh-contents))
+;; Bootstrap `use-package`
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure t)
 
-;; This installs elpa packages if we haven't done that yet
-(defun maybe-install-and-require (p)
-  (when (not (package-installed-p p))
-    (package-install p))
-  (require p))
-
-(setq desktop-restore-forces-onscreen nil)
-(setq magit-last-seen-setup-instructions "1.4.0")
+;; Pin all the things
+(setq package-pinned-packages
+      '((aggressive-indent . "melpa-stable")
+        (bind-key . "melpa")
+        (cider . "melpa-stable")
+        (cider-eval-sexp-fu . "melpa-stable")
+        (clj-refactor . "melpa-stable")
+        (clojure-mode . "melpa-stable")
+        (company . "melpa-stable")
+        ;; (dash . "melpa-stable")
+        ;; (diminish . "melpa-stable")
+        (epl . "melpa-stable")
+        (exec-path-from-shell . "melpa-stable")
+        (flx . "melpa-stable")
+        (flx-ido . "melpa-stable")
+        (git-commit . "melpa-stable")
+        (hydra . "melpa-stable")
+        (ido . "melpa-stable")
+        (ido-completing-read+ . "melpa-stable")
+        (ido-ubiquitous . "melpa-stable")
+        (ido-vertical-mode . "melpa-stable")
+        (flycheck-pos-tip . "melpa-stable")
+        (flycheck . "melpa-stable")
+        (highlight . "melpa") ;; woo! from the wiki https://www.emacswiki.org/emacs/highlight.el
+        (highlight-symbol . "melpa-stable")
+        (inflections . "melpa-stable")
+        (magit . "melpa-stable")
+        (magit-popup . "melpa-stable")
+        (multiple-cursors . "melpa-stable")
+        (org . "org")
+        (org-plus-contrib . "org")
+        (paredit . "melpa-stable")
+        (peg . "melpa-stable")
+        (pkg-info . "melpa-stable")
+        (pos-tip . "melpa-stable")
+        (projectile . "melpa-stable")
+        (rainbow-delimiters . "melpa-stable")
+        (s . "melpa-stable")
+        (seq . "elpa")
+        (smex . "melpa-stable")
+        (swiper . "melpa-stable")
+        (use-package . "melpa")
+        (with-editor . "melpa-stable")
+        (yasnippet . "melpa-stable")))
 
 ;; org-mode always needs to be installed in an emacs where it isn't loaded.
-(require 'org)
+(use-package org
+  :pin org
+  :ensure t)
 
-(org-babel-load-file (concat (getenv "HOME") "/.emacs.d/org/config.org"))
-(put 'erase-buffer 'disabled nil)
-(defvar clojure--prettify-symbols-alist nil)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("addfaf4c6f76ef957189d86b1515e9cf9fcd603ab6da795b82b79830eed0b284" "7c14c764f15a135f12824c322f2ec7b576fd06f1ca634c58538c6b47f53b069a" default)))
- '(helm-ff-lynx-style-map t)
- '(horizontal-scroll-bar-mode nil)
- '(package-selected-packages
-   (quote
-    (restclient git-link graphql company flycheck-color-mode-line flycheck markdown-mode tagedit org-magit helm-orgcard refheap gist js2-mode helm-cider yasnippet use-package smartscan rainbow-mode rainbow-delimiters projectile paredit multiple-cursors magit highlight-symbol helm git-gutter-fringe exec-path-from-shell discover diminish cyberpunk-theme cider buffer-move align-cljlet ace-jump-mode)))
- '(safe-local-variable-values
-   (quote
-    ((cider-refresh-after-fn . "user/go")
-     (cider-refresh-before-fn . "user/stop"))))
- '(scroll-bar-mode nil))
+(org-babel-load-file (concat user-emacs-directory "org/config.org"))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((((class color) (min-colors 89)) (:foreground "#d3d3d3" :background "#000000"))))
  '(helm-buffer-directory ((t (:background "gray8" :foreground "firebrick3"))))
  '(helm-ff-directory ((t (:background "gray17" :foreground "OrangeRed3"))))
  '(helm-selection ((t (:background "purple" :foreground "black" :underline nil)))))
+
+(setq desktop-restore-forces-onscreen nil)
+(setq magit-last-seen-setup-instructions "1.4.0")
+
+(put 'erase-buffer 'disabled nil)
+(defvar clojure--prettify-symbols-alist nil)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (flycheck-clj-kondo flycheck-color-mode-line flycheck markdown-mode js2-mode helm-cider cider clojure-mode smartscan yasnippet highlight-symbol rainbow-mode rainbow-delimiters paredit projectile buffer-move ace-jump-mode git-gutter-fringe magit helm cyberpunk-theme exec-path-from-shell discover multiple-cursors diminish company use-package))))
